2bbb79c0d6e9afc11f0f2668e93d2d79ed2f985a
qw87rt <130150002+qw87rt@users.noreply.github.com>
2023-07-12 13:12:57 +0800

    Add files via upload

---
+++ app/index.js
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <TouchableOpacity onPress={() => navigation.navigate('Settings')}>
+        <Text>Go to Settings</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+function SettingsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Settings Screen</Text>
+      <TouchableOpacity onPress={() => navigation.openDrawer()}>
+        <Text>Open Drawer</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer independent={true}>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Settings" component={SettingsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
